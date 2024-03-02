025b72a2e3c119b20f184d30e65c2a656e38246f
zonikhan <160257505+zonikhan@users.noreply.github.com>
2024-03-02 21:48:14 +0500

    Add files via upload

---
+++ variables/index.js
@@ -0,0 +1,4 @@
+//let name2:string ="rabia";
+//let age:number=1234
+//let day:boolean=true
+console.log("rabia", 123, true);
---
+++ variables/index.ts
@@ -0,0 +1,6 @@
+//let name2:string ="rabia";
+//let age:number=1234
+//let day:boolean=true
+
+
+console.log("rabia" ,123,true)
